5d1129f7628557600d4ebdfe8528fb8c9375dd39
Nickolas <eagle.dev.stack@gmail.com>
2025-08-11 21:50:00 +0400

    Реализация функции поиска

--- src/components/SearchField.tsx
+++ src/components/SearchField.tsx
@@ -6,14 +6,14 @@ interface Props {
   setSearchValue: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export default function SearchField({
-  searchFunc,
-  searchValue,
-  setSearchValue,
-}: Props) {
+export default function SearchField({ searchFunc }: Props) {
+
   return (
     <div className={styles.search}>
-      <input className={styles.search_input} />
+      <input
+        onChange={(event) => searchFunc(event.target.value)}
+        className={styles.search_input}
+      />
       <div className={styles.search_icon_wrapper}>
         <img className={styles.search_icon} src="/search.png" alt="search" />
       </div>
