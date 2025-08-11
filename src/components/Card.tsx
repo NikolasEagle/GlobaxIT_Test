fd745bd2fecd08d6403ffa5094335fd764b66341
Nickolas <eagle.dev.stack@gmail.com>
2025-08-11 21:40:26 +0400

    Реализация дизайна карточек

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,5 +6,19 @@ interface Props {
 }
 
 export default function Card({ user }: Props) {
-  return <div className={styles.card}>{user.id}</div>;
+  return (
+    <div className={styles.card}>
+      <h2>{user.name}</h2>
+      <div className={styles.contacts}>
+        <div>
+          <img className={styles.phone} src="/phone.png" alt="phone" />
+          <p>{user.phone}</p>
+        </div>
+        <div>
+          <img className={styles.email} src="/email.png" alt="email" />
+          <p>{user.email}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
