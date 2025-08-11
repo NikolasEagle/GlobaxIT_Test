0f1f8fea1544e56181b4a6cfa50303e8c4dc2d29
Nickolas <eagle.dev.stack@gmail.com>
2025-08-11 17:15:40 +0400

    Добавление стилей панели

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import styles from "./App.module.scss";
 import SearchField from "./components/SearchField";
+import CardsPanel from "./components/CardsPanel";
 
 export default function App() {
   return (
     <div className={styles.home}>
       <SearchField />
+      <CardsPanel />
     </div>
   );
 }
