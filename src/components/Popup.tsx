8d0d1cdb4c4b981e5d9ddc15dcec6c7371f1074a
Nickolas <eagle.dev.stack@gmail.com>
2025-08-11 22:56:46 +0400

    Верстка popup

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -10,7 +10,36 @@ export default function Popup({ isActive, user }: Props) {
   return (
     isActive && (
       <div className={styles.popup_shadow}>
-        <div className={styles.popup}></div>
+        <div className={styles.popup}>
+          <div className={styles.header}>
+            <h2>{user.name}</h2>
+            <div className={styles.close}></div>
+          </div>
+          <div className={styles.info}>
+            <div className={styles.desc}>
+              <p>Телефон:</p>
+              <p>Почта:</p>
+              <p>Дата приема:</p>
+              <p>Должность:</p>
+              <p>Подразделение:</p>
+            </div>
+            <div className={styles.data}>
+              <p>{user?.phone}</p>
+              <p>{user?.email}</p>
+              <p>{user?.hire_date}</p>
+              <p>{user?.position_name}</p>
+              <p>{user?.department}</p>
+            </div>
+          </div>
+          <div className={styles.add_info}>
+            <p className={styles.desc}>Дополнительная информация:</p>
+            <p className={styles.data}>
+              Разработчики используют текст в качестве заполнителя макта
+              страницы. Разработчики используют текст в качестве заполнителя
+              макта страницы.
+            </p>
+          </div>
+        </div>
       </div>
     )
   );
