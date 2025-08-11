a9fde6b71562904c1564ac70c43e0d372e6bd53e
Nickolas <eagle.dev.stack@gmail.com>
2025-08-11 20:55:50 +0400

    Реализация функции запроса через fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import styles from "./App.module.scss";
 import SearchField from "./components/SearchField";
 import CardsPanel from "./components/CardsPanel";
+import { useState } from "react";
 
 export default function App() {
+  const [content, setContent] = useState();
+
+  const [searchValue, setSearchValue] = useState<string>("");
+
+  const search = async (query: string): Promise<void> => {
+    const url = "http://localhost:3000";
+
+    try {
+      const response = await fetch(`${url}?term=${query}`);
+
+      const body = await response.json();
+
+      setContent(body);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className={styles.home}>
-      <SearchField />
-      <CardsPanel />
+      <SearchField
+        searchFunc={search}
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
+      <CardsPanel content={content} />
     </div>
   );
 }
