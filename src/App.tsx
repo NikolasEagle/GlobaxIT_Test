cf11a474e0209d4b374561429ed81fe09eb60305
Nickolas <eagle.dev.stack@gmail.com>
2025-08-11 17:02:57 +0400

    Верстка компонента поисковой строки

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import styles from "./App.module.scss";
+import SearchField from "./components/SearchField";
 
 export default function App() {
-  return <div className={styles.home}></div>;
+  return (
+    <div className={styles.home}>
+      <SearchField />
+    </div>
+  );
 }
--- src/components/SearchField.tsx
+++ src/components/SearchField.tsx
@@ -1,5 +1,12 @@
 import styles from "./SearchField.module.scss";
 
 export default function SearchField() {
-  return <div className={styles.home}></div>;
+  return (
+    <div className={styles.search}>
+      <input className={styles.search_input} />
+      <div className={styles.search_icon_wrapper}>
+        <img className={styles.search_icon} src="/search.png" alt="search" />
+      </div>
+    </div>
+  );
 }
