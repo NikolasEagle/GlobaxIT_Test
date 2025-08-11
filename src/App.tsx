f6216d017c57f1eac8aedb9ad22e77450b516b14
Nickolas <eagle.dev.stack@gmail.com>
2025-08-11 14:17:56 +0400

    Внедрение sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import styles from "./App.scss";
 
 function App() {
-
-  return (
-    <>
-      
-    </>
-  )
+  return <main></main>;
 }
 
-export default App
+export default App;
